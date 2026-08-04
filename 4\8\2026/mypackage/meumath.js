30768d8721b33ff012f327fba486d3f8fe537c71
heitorigt <souza.heitor@aluno.ifsp.edu.br>
2026-08-04 14:42:02 -0300

    comit
    o resto q ficou faltando

---
+++ 4\8\2026/mypackage/meumath.js
@@ -0,0 +1,20 @@
+function som (a,b) {
+    return a+b;
+}
+
+function mult (a,b) {
+    return a*b;
+}
+
+function sub (a,b) {
+    return a-b;
+}
+
+function div (a,b) {
+    return a/b;
+}
+
+function pot (a,b) {
+    return a**b;
+}
+module.exports= {som, mult, sub, div, pot}
